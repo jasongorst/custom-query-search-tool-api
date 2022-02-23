02bc73e6f6759c2fcad68eda1d533a9ebc9d7582
Jason Gorst <jason.gorst@me.com>
2022-02-22 22:34:58 -0600

    override default git branch in deploy.rb.

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ set :application, "cqstapi"
 set :repo_url, "git@github.com:jasongorst/custom-query-search-tool-api.git"
 
 # Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
 set :deploy_to, "/var/www/cqstapi"
