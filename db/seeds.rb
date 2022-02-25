c1f26b3236706d93cfe55e2d93ff49b56252abe8
Jason Gorst <jason.gorst@me.com>
2022-02-24 19:14:19 -0600

    explicitly DateTime.parse order_time from csv in seeds.rb.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
 require 'csv'
 
 CSV.foreach(Rails.root.join('db/TransactionData.csv'), headers: true) do |row|
@@ -13,7 +6,7 @@ CSV.foreach(Rails.root.join('db/TransactionData.csv'), headers: true) do |row|
       restaurant_id: row[0],
       bus_dt: Date.strptime(row[1], '%m/%d/%Y'),
       order_number: row[2],
-      order_time: row[3],
+      order_time: DateTime.parse(row[3]),
       transaction_total_amount: row[4],
       transaction_net_amount: row[5],
       item_sold_qty: row[6],
